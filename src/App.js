ca327bf6ce0e657652e9c469192515497002eff6
krisyam <18104057@usc.edu.ph>
2024-02-22 01:59:43 +0800

    Checking if netlify redirects work

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Header } from './components/Header'
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Header />
                 <div className="content">
