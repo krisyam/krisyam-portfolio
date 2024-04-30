a566caee108b484b06e75d1f2184bf14bfea05aa
krisyam <18104057@usc.edu.ph>
2024-04-30 23:36:40 +0800

    Update FooterComponent.js

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,8 +1,6 @@
-import { useLocation } from 'react-router-dom'
 import { ExternalLinks } from './ExternalLinksComponent'
 
 export function FooterComponent() {
-    const location = useLocation().pathname
     return (
         <footer 
             // style={{ display: location === '/' ? 'none' : '' }}
