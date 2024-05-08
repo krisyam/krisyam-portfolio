e6a0c8537c8933ef60daa6b498333ee82dca4299
krisyam <18104057@usc.edu.ph>
2024-05-09 01:23:02 +0800

    Update ProjectsComponent.js

--- src/components/projects/ProjectsComponent.js
+++ src/components/projects/ProjectsComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 export default function ProjectsComponent() {
     function redirectToGoogleDrive() {
-        window.location.href = 'https://drive.google.com/file/d/1ghRyWeDXQ3a-CBSTgSeMZKSG27IULvVo/view?usp=sharing';
+        window.location.href = 'https://drive.google.com/file/d/1rvILHbBQs0vbPMRa7tx4dl4ppTQLvTbc/view?usp=sharing';
     }
     return (
         <>
